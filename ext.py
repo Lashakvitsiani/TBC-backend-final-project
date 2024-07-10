9a80a617e67e51d038a5b9ce2c771870b05c224c
Lashakvitsiani <90346977+Lashakvitsiani@users.noreply.github.com>
2024-07-10 14:04:38 +0400

    Update ext.py

--- ext.py
+++ ext.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "123"
@@ -10,4 +10,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
-migrate = Migrate(app, db)
+
