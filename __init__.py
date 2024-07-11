9eb8a2d05962c95c26c849d8275a3def12ea4828
Lashakvitsiani <90346977+Lashakvitsiani@users.noreply.github.com>
2024-07-11 14:17:10 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from models import Product, User, Like
 with app.app_context():
     db.create_all()
 
-    admin_user = User("admin12", "lashakvitsiani5@gmail.com", "test1234", "admin")
+    admin_user = User("Lasha", "lashakvitsiani5@gmail.com", "test1234", "admin")
     db.session.add(admin_user)
     db.session.commit()
+  
